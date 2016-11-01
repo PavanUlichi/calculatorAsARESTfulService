204c5158edc7fc9e459fcb2aba34c8d71a6fefbf
root <root@ip-172-31-29-10.us-west-2.compute.internal>
2016-11-01 22:57:25 +0000

    adding the api /api/add to the existing file

--- flaskapp.py
+++ flaskapp.py
@@ -9,6 +9,15 @@ app = Flask(__name__)
 def get_tasks():
     return render_template('index.html')
 
+@app.route('/api/add', methods=['GET'])
+def get_add():
+    a = request.args.get('num1')
+    a = int(a)
+    b = request.args.get('num2')
+    b = int(b)
+    c = a+b
+    return jsonify({"sum": c})
+    
 if __name__ == '__main__':
-    app.run()
+        app.run()
 
