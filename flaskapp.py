9d4c5c048fa312b39228e3fa8daf905fd232b6f5
root <root@ip-172-31-29-10.us-west-2.compute.internal>
2016-11-02 04:12:36 +0000

    code after the implementation of javascript

--- flaskapp.py
+++ flaskapp.py
@@ -5,61 +5,44 @@ from flask import abort
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def get_tasks():
     return render_template('index.html')
 
-@app.route('/api/add', methods=['GET'])
+@app.route('/api/add', methods=['GET','POST'])
 def get_add():
     a = request.args.get('num1')
     a = float(a)
     b = request.args.get('num2')
     b = float(b)
     c = a+b
-    return jsonify({"sum": c})
+    return jsonify({"result": c})
 
-@app.route('/api/sub', methods=['GET'])
+@app.route('/api/sub', methods=['GET','POST'])
 def get_sub():
     a = request.args.get('num1')
     a = float(a)
     b = request.args.get('num2')
     b = float(b)
     c = a-b
-    return jsonify({"difference": c})
+    return jsonify({"result": c})
 
-@app.route('/api/mul', methods=['GET'])
+@app.route('/api/mul', methods=['GET','POST'])
 def get_mul():
     a = request.args.get('num1')
     a = float(a)
     b = request.args.get('num2')
     b = float(b)
     c = a*b
-    return jsonify({"product": c})
+    return jsonify({"result": c})
 
-@app.route('/api/div', methods=['GET'])
+@app.route('/api/div', methods=['GET','POST'])
 def get_div():
     a = request.args.get('num1')
     a = float(a)
     b = request.args.get('num2')
     b = float(b)
     c = a/b
-    return jsonify({"ratio": c})
-
-@app.route('/api', methods=['GET'])
-def get_opr():
-    a = request.args.get('num1')
-    a = float(a)
-    b = request.args.get('num2')
-    b = float(b)
-    o = request.args.get('opp')
-    if(o == 'add'):
-        c = a+b
-    elif(o == 'sub'):
-        c = a-b
-    elif(o == 'mul'):
-        c = a*b
-    else:
-        c = a/b
     return jsonify({"result": c})
     
 if __name__ == '__main__':
